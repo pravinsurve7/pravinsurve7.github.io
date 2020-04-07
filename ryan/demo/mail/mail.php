2eaff77e9bf6cd9570d4e1fb21c9d21ae30d735b
survepravin <pravinnv7@gmail.com>
2020-04-07 22:26:47 +0530

    contact form code

--- ryan/demo/mail/mail.php
+++ ryan/demo/mail/mail.php
@@ -6,6 +6,8 @@ $formcontent=" From: $name \n Message: $message";
 $recipient = "pravinnv7@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader);
+header("Location: index.html");
 // echo "Thank You!" . " -" . "<a href='#' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!";
 ?>
